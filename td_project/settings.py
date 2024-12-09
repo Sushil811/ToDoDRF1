45b173976aa9dd65a158194fa9a68d55b04dd21a
Sushil Prajapati <70131939+Sushil811@users.noreply.github.com>
2024-12-09 12:18:14 +0530

    Update settings.py
    Host Allow .vercel.app to *

--- td_project/settings.py
+++ td_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e6mxl8=_ehga(yi7))4jujzo-_f%7@nffh^6+ro9e17c23mfis
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
